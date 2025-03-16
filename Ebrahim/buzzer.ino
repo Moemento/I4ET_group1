004b83c4ba1e517a51fb66039fd1175cc2f9b7a3
ebrahimtahmasebiboldaji <ebrahim22thmb@gmail.com>
2025-03-16 15:35:13 +0100

    buzzer.ino

--- Ebrahim/buzzer.ino
+++ Ebrahim/buzzer.ino
@@ -1,26 +1,17 @@
-#include <pitches.h>
+const int buzzer = 8; //buzzer to arduino pin 9
 
 
+void setup(){
+   
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
 
-// Notes in the melody
-int melody[] = {
-  NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6
-};
-
-int duration = 500;  // 500 milliseconds
-
-void setup() {
-  pinMode(1, OUTPUT); // Set the buzzer pin as an output
 }
 
-void loop() {
-  //for (int thisNote = 0; thisNote < 8; thisNote++) {
-    // Play the note on the buzzer    tone(D1, melody[5], duration);
-    
-    // Wait before playing the next note
-    delay(10);
-  //}
-
-  // Pause before repeating the melody
-  delay(10000);
+void   loop(){
+ 
+  tone(buzzer, 1000); // Send 1KHz sound signal...
+  delay(1000);         // ...for 1 sec
+  noTone(buzzer);     // Stop sound...
+  delay(1000);         // ...for 1sec
+  
 }
